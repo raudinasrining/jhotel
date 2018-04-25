4bf07e8eebc949eca61bf9923598c96cb3856d89
raudinasrining <raudina.asrining@ui.ac.id>
2018-04-25 22:25:16 +0700

    modul 8 TP

---
+++ src/main/java/jhotel/CustomerController.java
@@ -0,0 +1,32 @@
+package jhotel;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class CustomerController {
+
+    @RequestMapping("/")
+    public String indexPage(@RequestParam(value="name", defaultValue="world") String name) {
+        return "Hello " + name;
+    }
+
+    @RequestMapping(value = "/newcustomer", method = RequestMethod.POST)
+    public Customer newCust(@RequestParam(value="name") String name,
+                            @RequestParam(value="email") String email) {
+        Customer customer = new Customer(name, 10, 10, 10, email);
+        try {
+            DatabaseCustomer.addCustomer(customer);
+        } catch(Exception ex) {
+            ex.getMessage();
+            return null;
+        };
+        return customer;
+    }
+
+    @RequestMapping("/getcustomer/{id}")
+    public Customer getCust(@PathVariable int id) {
+        Customer customer = DatabaseCustomer.getCustomer(id);
+        return customer;
+    }
+
+}
